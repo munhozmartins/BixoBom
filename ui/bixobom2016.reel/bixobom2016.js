9ad19dffa00dbf804e25874ae00f07dcc02fe845
munhozmartins <noreply>
2016-04-20 00:41:13 +0000

    Add component bixobom2016

---
+++ ui/bixobom2016.reel/bixobom2016.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/bixobom2016.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Bixobom2016
+ * @extends Component
+ */
+exports.Bixobom2016 = Component.specialize(/** @lends Bixobom2016# */ {
+    constructor: {
+        value: function Bixobom2016() {
+            this.super();
+        }
+    }
+});
